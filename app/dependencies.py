77acde155e61fbfa2592c27b05a4803b66f57371
Camilo Caceres <camiloac97@gmail.com>
2024-11-28 12:18:07 -0500

    refactor: Replace hardcoded tokens with environment variables for improved security

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,13 +1,12 @@
 from typing import Annotated
-
 from fastapi import Header, HTTPException
-
+import os
 
 async def get_token_header(x_token: Annotated[str, Header()]):
-    if x_token != "fake-super-secret-token":
+    if x_token != os.getenv("API_SECRET_KEY"):
         raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 
 async def get_query_token(token: str):
-    if token != "jessica":
-        raise HTTPException(status_code=400, detail="No Jessica token provided")
+    if token != os.getenv("QUERY_SECRET_KEY"):
+        raise HTTPException(status_code=400, detail="Invalid query token")
